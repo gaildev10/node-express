c02e80668d32a4c4dc136b128eaf10171166c8b4
GailDev10 <GailDev10@gmail.com>
2022-06-09 17:15:15 -0400

    completed node.js-express homework

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 
 
 const express = require ('express')
+const res = require('express/lib/response')
 const app = express()
+const morgan = require('morgan')
 const PORT = 3001
+
+app.use(express.json())
+app.use(morgan('tiny'))
+
+morgan.token('object', function (req, res) {
+  return `${JSON.stringify(req.body)}`
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
 
 let persons = [
     { 
@@ -46,6 +57,42 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(entry => entry.id !==id)
+  res.status(204).end()
+})
+
+
+const generatedId = () => { 
+  const maxID = persons.length > 0
+  ? Math.max( ...persons.map(n => n.id))
+  : 0
+  return maxID + 1
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if(!body.name){
+    return res.status(400).json({error: 'name is missing'}
+    )
+  }
+  if(!body.number ){
+    return res.status(400).json({error: `number is missing`})
+  }
+  if(persons.some(entry => entry.name === body.name)){
+    return res.status(409).json({error: `name must be unique`})
+  }
+
+let entry = {
+  id: generatedId(),
+  name: body.name,
+  number: body.number
+}
+
+persons.push(entry)
+res.json(entry)
+})
 
 app.listen(PORT, () =>{ 
 console.log(`Server is active on  ${PORT}`)
